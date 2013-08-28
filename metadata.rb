46b1516a20a82ec3ac968dd82ecde4139b6632dd
Seth Vargo <sethvargo@gmail.com>
2013-08-28 18:35:52 -0400

    Bump devodd release

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "cookbooks@opscode.com"
 license          "Apache 2.0"
 description      "Installs/Configures Microsoft SQL Server 2008 R2"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.2.2"
+version          "1.2.3"
 supports         "windows"
 depends          "openssl"
 depends          "windows", ">= 1.2.6"
