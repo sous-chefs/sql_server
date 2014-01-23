203331849ef0fafa4fae9df2f4b10b0489196e1b
Eric Blevins <eblevins@kmionline.com>
2014-01-23 14:13:41 -0500

    INSTALLSHAREDWOWDIR can be configured with node['sql_server']['shared_wow_dir']

--- attributes/server.rb
+++ attributes/server.rb
@@ -23,7 +23,7 @@ default['sql_server']['port']           = 1433
 
 default['sql_server']['instance_name']  = 'SQLEXPRESS'
 default['sql_server']['instance_dir']   = 'C:\Program Files\Microsoft SQL Server'
-
+default['sql_server']['shared_wow_dir']   = 'C:\Program Files (x86)\Microsoft SQL Server'
 default['sql_server']['feature_list'] = 'SQLENGINE,REPLICATION,SNAC_SDK'
 
 default['sql_server']['server']['installer_timeout'] = 1500
