ec0d0f2395b7781a932cb8116684b7d4594e1c92
Sean OMeara <sean@chef.io>
2015-02-18 18:22:42 -0500

    updating to work with latest openssl

--- recipes/server.rb
+++ recipes/server.rb
@@ -18,7 +18,7 @@
 # limitations under the License.
 #
 
-::Chef::Recipe.send(:include, Chef::OpenSSL::Password)
+::Chef::Recipe.send(:include, OpenSSLCookbook::Password)
 
 service_name = node['sql_server']['instance_name']
 if node['sql_server']['instance_name'] == 'SQLEXPRESS'
