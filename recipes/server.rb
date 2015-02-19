787037a793cab4f64ffc35bc2178e3e3d3ccfb60
Sean OMeara <sean@chef.io>
2015-02-19 15:27:56 -0800

    reverting OpenSSL module namespace change

--- recipes/server.rb
+++ recipes/server.rb
@@ -18,7 +18,7 @@
 # limitations under the License.
 #
 
-::Chef::Recipe.send(:include, Chef::OpenSSL::Password)
+::Chef::Recipe.send(:include, Opscode::OpenSSL::Password)
 
 service_name = node['sql_server']['instance_name']
 if node['sql_server']['instance_name'] == 'SQLEXPRESS'
