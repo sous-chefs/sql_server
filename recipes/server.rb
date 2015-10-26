49d72ffd770ca74e42cfe2fbc57d9c219e81bdc0
Taliesin Sisson <taliesins@interxion.com>
2015-10-26 14:57:59 +0000

    Add installation support for ISOs

--- recipes/server.rb
+++ recipes/server.rb
@@ -19,6 +19,7 @@
 #
 
 ::Chef::Recipe.send(:include, Opscode::OpenSSL::Password)
+::Chef::Recipe.send(:include, Windows::Helper)
 
 service_name = node['sql_server']['instance_name']
 if node['sql_server']['instance_name'] == 'SQLEXPRESS'
@@ -63,6 +64,59 @@ package_checksum = node['sql_server']['server']['checksum'] ||
                    SqlServer::Helper.sql_server_checksum(version, x86_64) ||
                    Chef::Application.fatal!("No package checksum matches '#{version}'. node['sql_server']['server']['checksum'] must be set or node['sql_server']['version'] must match a supported version.")
 
+is_sqlserver_installed = is_package_installed?(package_name)
+
+filename = File.basename(package_url).downcase
+iso_install = File.extname(filename) == ".iso"
+download_path = "#{Chef::Config['file_cache_path']}/#{filename}"
+
+if iso_install
+  remote_file download_path do
+    source package_url
+    checksum package_checksum
+    not_if {is_sqlserver_installed}
+  end
+  
+  mount_log_path = "#{Chef::Config['file_cache_path']}/#{filename}_Mount_Log.txt"
+  mount_drive_path = "#{Chef::Config['file_cache_path']}/#{filename}_Mount_Drive.txt"
+  powershell_script "Mount #{filename}" do
+    code <<-EOH
+      Mount-DiskImage -ImagePath "#{download_path}"
+          if ($? -eq $True)
+      {
+        echo "Success: #{filename} was mounted successfully." > #{mount_log_path}
+        $SQL_Server_ISO_Drive_Letter = @(gwmi -Class Win32_LogicalDisk | ?{$_.VolumeName -eq 'SqlServer'} | %{$_.DeviceId})[0]
+        $url = "$SQL_Server_ISO_Drive_Letter/setup.exe"
+
+        echo $url > #{mount_drive_path}
+        exit 0;
+      }
+      
+      if ($? -eq $False)
+          {
+        echo "Fail: #{filename} was not mount successfully." > #{mount_log_path}
+        exit 2;
+          }
+      EOH
+    not_if {is_sqlserver_installed}
+  end
+
+  ruby_block 'Get installation url' do
+    block do
+      encoding_options = {
+          :invalid           => :replace,  # Replace invalid byte sequences
+          :undef             => :replace,  # Replace anything not defined in ASCII
+          :replace           => '',        # Use a blank for those replacements
+          :universal_newline => true       # Always break lines with \n
+        }
+        package_url = File.read(mount_drive_path).encode(Encoding.find('ASCII'), encoding_options)
+    end
+    not_if {is_sqlserver_installed}
+  end
+
+  package_checksum = ""
+end
+
 windows_package package_name do
   source package_url
   checksum package_checksum
@@ -70,10 +124,12 @@ windows_package package_name do
   installer_type :custom
   options "/q /ConfigurationFile=#{config_file_path}"
   action :install
+  not_if {is_sqlserver_installed}
 end
 
 service service_name do
   action :nothing
+  not_if {is_sqlserver_installed}
 end
 
 # set the static tcp port
@@ -82,6 +138,16 @@ registry_key static_tcp_reg_key do
           { name: 'TcpDynamicPorts', type: :string, data: '' }]
   recursive true
   notifies :restart, "service[#{service_name}]", :immediately
+  not_if {is_sqlserver_installed}
 end
 
 include_recipe 'sql_server::client'
+
+if iso_install
+  powershell_script 'Dismount #{filename}' do
+    code <<-EOH
+      Dismount-DiskImage -ImagePath "#{download_path}"
+      EOH
+    not_if {is_sqlserver_installed}
+  end
+end
