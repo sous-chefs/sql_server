eb865fbf38211415d3f39028c6fc5ec4626839c5
Taliesin Sisson <taliesins@interxion.com>
2015-10-26 09:21:55 +0000

    Remove unused gem

--- recipes/client.rb
+++ recipes/client.rb
@@ -37,9 +37,3 @@ unless node.recipe?('sql_server::server')
   end
 
 end
-
-# used by SQL Server providers for
-# database and database_user resources
-gem_package 'tiny_tds' do
-  action :install
-end
