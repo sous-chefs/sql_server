4ad1496875733463881461ea84b45830c9cd36af
Taliesin Sisson <taliesins@interxion.com>
2015-11-26 09:51:43 +0000

    Need to ensure folder exists. 7zip will only create 1 directory deep.

--- recipes/server.rb
+++ recipes/server.rb
@@ -78,6 +78,10 @@ end
 
 iso_extraction_dir = "#{Chef::Config['file_cache_path']}/sql_server/#{package_checksum}"
 
+directory iso_extraction_dir do
+  recursive true
+end
+
 execute 'extract_iso' do
   command "#{File.join(node['7-zip']['home'], '7z.exe')} x -y -o\"#{iso_extraction_dir}\" #{download_path}"
   only_if { is_iso && !(::File.directory?(iso_extraction_dir)) }
