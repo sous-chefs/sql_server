ee14312c197232f71ba9b6c0e5b04bb1ab6d037f
Tim Smith <tsmith84@gmail.com>
2016-01-06 16:08:52 -0800

    Remove openssl dep

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,6 @@ description      'Installs/Configures Microsoft SQL Server 2008 R2'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '2.4.0'
 supports         'windows'
-depends          'openssl'
 depends          'windows', '>= 1.2.6'
 
 source_url 'https://github.com/chef-cookbooks/sql_server' if respond_to?(:source_url)
