24a248bbf9a59db52194cb1d06522f369d741470
Tim Smith <tsmith@chef.io>
2016-11-18 16:36:15 -0800

    Don’t fail if the version specified is an int and not a string
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- libraries/helper.rb
+++ libraries/helper.rb
@@ -25,25 +25,25 @@ module SqlServer
     extend Chef::Mixin::ShellOut
 
     def self.reg_version_string(version)
-      case version
+      case version.to_s # to_s to make sure someone didn't pass us an int
       when '2008' then 'MSSQL10.'
       when '2008R2' then 'MSSQL10_50.'
       when '2012' then 'MSSQL11.'
       when '2014' then 'MSSQL12.'
       when '2016' then 'MSSQL13.'
-      else raise "Unsupported sql_server version '#{node['sql_server']['version']}'"
+      else raise "Unsupported sql_server version '#{version}'. Please open a PR to add support for this version."
       end
     end
 
     def self.install_dir_version(version)
-      case version
-      when /2008/
+      case version.to_s # to_s to make sure someone didn't pass us an int
+      when '2008'
         '100'
-      when /2012/
+      when '2012'
         '110'
-      when /2014/
+      when '2014'
         '120'
-      when /2016/
+      when '2016'
         '130'
       else
         Chef::Application.fatal!("SQL Server version #{version} not supported. Please open a PR to add support for this version.")
@@ -57,7 +57,7 @@ module SqlServer
 
     def self.sql_server_url(version, x86_64)
       if x86_64
-        case version
+        case version.to_s # to_s to make sure someone didn't pass us an int
         when '2008R2'
           'https://download.microsoft.com/download/D/1/8/D1869DEC-2638-4854-81B7-0F37455F35EA/SQLEXPR_x64_ENU.exe'
         when '2012'
@@ -68,7 +68,7 @@ module SqlServer
           'https://download.microsoft.com/download/9/A/E/9AE09369-C53D-4FB7-985B-5CF0D547AE9F/SQLServer2016-SSEI-Expr.exe'
         end
       else
-        case version
+        case version.to_s
         when '2008R2'
           'http://download.microsoft.com/download/D/1/8/D1869DEC-2638-4854-81B7-0F37455F35EA/SQLEXPR32_x86_ENU.exe'
         when '2012'
@@ -81,7 +81,7 @@ module SqlServer
 
     def self.sql_server_package_name(version, x86_64)
       if x86_64
-        case version
+        case version.to_s # to_s to make sure someone didn't pass us an int
         when '2008R2'
           'Microsoft SQL Server 2008 R2 (64-bit)'
         when '2012'
@@ -92,7 +92,7 @@ module SqlServer
           'Microsoft SQL Server 2016 (64-bit)'
         end
       else
-        case version
+        case version.to_s
         when '2008R2'
           'Microsoft SQL Server 2008 R2 (32-bit)'
         when '2012'
@@ -105,7 +105,7 @@ module SqlServer
 
     def self.sql_server_checksum(version, x86_64)
       if x86_64
-        case version
+        case version.to_s # to_s to make sure someone didn't pass us an int
         when '2008R2'
           '8ebf6bdd805f3326d5b9a35a129af276c7fe99bfca64ac0cfe0ffc66311dfe09'
         when '2012'
@@ -116,7 +116,7 @@ module SqlServer
           'bdb84067de0187234673de73216818fcfd774501307e84b8cba327b948ef4ca6'
         end
       else
-        case version
+        case version.to_s
         when '2008R2'
           '602d6525261ef65612ba713daddfbe7c73869dfdf19988db31fa2e66c0d38c04'
         when '2012'
