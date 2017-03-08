e496c3c3988e635a7fb122498a39891f67bd1865
Tim Smith <tsmith@chef.io>
2017-03-08 12:03:41 -0800

    Add appveyer integration testing of the client recipe (#86)
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -9,4 +9,4 @@ supports         'windows'
 depends          'windows', '>= 2.0'
 source_url       'https://github.com/chef-cookbooks/sql_server'
 issues_url       'https://github.com/chef-cookbooks/sql_server/issues'
-chef_version     '>= 12.6'
+chef_version     '>= 12.6' if respond_to?(:chef_version)
