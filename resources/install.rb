cb8ea61667dc89a7b9e183c5dd424895787f959c
Tim Smith <tsmith@chef.io>
2017-10-27 10:19:39 -0700

    Add copyright header to the resource
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- resources/install.rb
+++ resources/install.rb
@@ -1,3 +1,22 @@
+
+#
+# Cookbook:: sql_server
+# Resource:: install
+#
+# Copyright:: 2017, Chef Software, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 property :sql_reboot, [true, false], default: true
 property :security_mode, String, equal_to: ['Windows Authenication', 'Mixed Mode Authentication'], default: 'Windows Authenication'
 property :sa_password, String
@@ -143,6 +162,7 @@ end
 
 action_class do
   include ::SqlServer::Helper
+
   def build_admin_list(admin_list)
     if admin_list.is_a? Array
       admin_list.map { |account| %("#{account}") }.join(' ')
