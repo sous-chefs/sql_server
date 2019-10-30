4f8196fa7d53ce737e1ae5a5640ceb6a3debc843
Tim Smith <tsmith84@gmail.com>
2019-10-30 13:51:23 -0700

    Remove unused long_description metadata
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,6 @@ maintainer       'Chef Software, Inc.'
 maintainer_email 'cookbooks@chef.io'
 license          'Apache-2.0'
 description      'Installs/Configures Microsoft SQL Server'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '5.5.1'
 supports         'windows'
 depends          'windows', '>= 3.0'
